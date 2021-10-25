6490fd2bff930f18235d48884291cca316bb5c50
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-25 12:05:10 +0300

    fork: add Account Page

---
+++ src/pages/AccountPage/AccountPage.tsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from "react";
+
+import Navigation from "../../components/Navigation";
+import Account from "../../components/Account";
+
+const AccountPage = () => {
+  return (
+    <Fragment>
+      <Navigation />
+      <Account />
+    </Fragment>
+  );
+};
+
+export default AccountPage;
