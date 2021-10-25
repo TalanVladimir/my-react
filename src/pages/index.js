ffd13cc0900a454c64888fdf814047ed6dd7622c
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-25 12:05:34 +0300

    fork: add Pages for export

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import LoginPage from "./LoginPage";
+import BuyPage from "./BuyPage";
+import FinanciesPage from "./FinanciesPage";
+import AccountPage from "./AccountPage";
+import NotFoundPage from "./NotFoundPage";
+
+export { LoginPage, BuyPage, FinanciesPage, AccountPage, NotFoundPage };
