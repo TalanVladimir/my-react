b533d01fc9209c3a017a67cea7728d9ddbfa1f80
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-25 12:11:24 +0300

    fork: add BrowserRouter, Pages

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,12 +1,32 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 
-import Header from "../Header";
+import {
+  LoginPage,
+  BuyPage,
+  FinanciesPage,
+  AccountPage,
+  NotFoundPage,
+} from "../../pages";
+
+import ErrorBoundry from "../ErrorBoundry";
+
+import "./styles.scss";
 
 const App = () => {
   return (
-    <div>
-      <Header></Header>asd
-    </div>
+    <ErrorBoundry>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={LoginPage} />
+          <Route exact path='/logout' component={LoginPage} />
+          <Route path='/buy' component={BuyPage} />
+          <Route path='/financies' component={FinanciesPage} />
+          <Route path='/account' component={AccountPage} />
+          <Route path='*' component={NotFoundPage} />
+        </Switch>
+      </BrowserRouter>
+    </ErrorBoundry>
   );
 };
 
