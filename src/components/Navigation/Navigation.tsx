90425a168b982786ec612b52116d9923dbf7e454
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-26 16:49:14 +0300

    fork: add firebase logout

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 import Icon from "../../images/logo.png";
+
+import { logout } from "../../services/firebase";
 
 import "./styles.scss";
 
 const Navigation = () => {
+  const isGit = document.location.host === "talanvladimir.github.io";
+  const history = useHistory();
+
   return (
     <Navbar sticky='top' bg='dark' variant='dark' className='navbar-inverse'>
       <Container className='flex-wrap'>
@@ -42,7 +46,16 @@ const Navigation = () => {
             </Nav.Link>
           </Nav.Item>
           <Nav.Item>
-            <Nav.Link as={Link} to='/logout'>
+            <Nav.Link
+              onClick={() =>
+                logout().then(() => {
+                  setTimeout(
+                    () => history.replace(isGit ? "/new-react/" : "/login"),
+                    100
+                  );
+                })
+              }
+            >
               Logout
             </Nav.Link>
           </Nav.Item>
