b2a046b6794335b5bace8d475c5e9502b17c4ce6
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-26 09:36:02 +0300

    refactor: add redirect, delete 404 page

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,5 @@ import LoginPage from "./LoginPage";
 import BuyPage from "./BuyPage";
 import FinanciesPage from "./FinanciesPage";
 import AccountPage from "./AccountPage";
-import NotFoundPage from "./NotFoundPage";
 
-export { LoginPage, BuyPage, FinanciesPage, AccountPage, NotFoundPage };
+export { LoginPage, BuyPage, FinanciesPage, AccountPage };
