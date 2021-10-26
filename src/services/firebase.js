1b082d48bce25c9218f8919925c43d07b9cf6231
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-26 16:47:58 +0300

    fork: add firebase auth with login methods

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,9 +1,45 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 import firebaseConfig from "./firebase.config";
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
 
-export { db };
+const auth = getAuth(firebaseApp);
+
+import { createUserWithEmailAndPassword } from "firebase/auth";
+const register = async (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+import { signInWithEmailAndPassword } from "firebase/auth";
+const login = async (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in
+      const user = userCredential.user;
+      // alert(JSON.stringify(user));
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+};
+
+import { signOut } from "firebase/auth";
+const logout = async () => {
+  signOut(auth)
+    .then(() => {})
+    .catch((error) => {});
+};
+
+export { db, auth, login, logout };
