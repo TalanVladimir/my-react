42790c19e058afccd325f29887294e20dd3e2050
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-26 16:48:30 +0300

    fork: add Register

---
+++ src/components/Register/Register.tsx
@@ -0,0 +1,63 @@
+import React, { useState, useEffect } from "react";
+
+import { Form, Button } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
+
+import { useAuthState } from "react-firebase-hooks/auth";
+import { auth, login } from "../../services/firebase";
+
+import "./styles.scss";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [user, loading, error] = useAuthState(auth);
+  const history = useHistory();
+
+  useEffect(() => {
+    if (loading) {
+      return;
+    }
+
+    if (user) history.replace("/buy");
+  }, [user, loading]);
+
+  const handleSubmit = (event: { preventDefault: () => void }) => {
+    event.preventDefault();
+    login(email, password);
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Form.Group>
+        <Form.Control
+          type='text'
+          placeholder='Email'
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </Form.Group>
+
+      <Form.Group>
+        <Form.Control
+          type='password'
+          placeholder='Password'
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </Form.Group>
+      <Button variant='primary' type='submit'>
+        Register
+      </Button>
+      <Button
+        variant='secondary'
+        type='button'
+        onClick={() => {
+          history.replace("/login");
+        }}
+      >
+        Login Page
+      </Button>
+    </Form>
+  );
+};
+
+export default Register;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
+import RegisterPage from "./RegisterPage";
 import LoginPage from "./LoginPage";
 import BuyPage from "./BuyPage";
 import FinanciesPage from "./FinanciesPage";
 import AccountPage from "./AccountPage";
 
-export { LoginPage, BuyPage, FinanciesPage, AccountPage };
+export { RegisterPage, LoginPage, BuyPage, FinanciesPage, AccountPage };
