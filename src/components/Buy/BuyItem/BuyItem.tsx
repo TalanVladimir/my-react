2ec408caff64efe16016f6aad18bd964be36ed1e
Vladimir Talan <talan.vladimir@gmail.com>
2021-10-27 15:44:39 +0300

    fork: add table item

--- src/components/Buy/BuyItem/BuyItem.tsx
+++ src/components/Buy/BuyItem/BuyItem.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
+
+import { Form, Button } from "react-bootstrap";
 
 import "./styles.scss";
 
-const BuyItem = () => {
-  return <div>buy item</div>;
+const BuyItem = (props: any) => {
+  return (
+    <tr className='buyItem' key={props.children.id}>
+      <th scope='row'>{props.index + 1}</th>
+      <td>{props.children.category}</td>
+      <td>{props.children.product}</td>
+      <td>{props.children.price}</td>
+      <td>
+        <Button variant='primary' type='button'>
+          Submit
+        </Button>
+      </td>
+    </tr>
+  );
 };
 
 export default BuyItem;
